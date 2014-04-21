457dbc6929265978d3914c4d9c1bfda567727c90
efrainastudillo <efrain.astudillo58@gmail.com>
2014-04-20 23:06:10 -0500

    Inserted a multiplication operation on main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include"Clase.h"
+
+
 int main(int argc, char * argv[]){
+  int result = 4 * 6;
+ std::cout<<"resultado = "<< result<<std::endl;
 //your code here
   std::cout<<"Hello world!"<<std::endl;
  return 0;
